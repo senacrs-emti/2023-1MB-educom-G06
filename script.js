7889928916cce2c4146460ca79799a0ba3af3e48
joaocidade <134340858+joaocidade@users.noreply.github.com>
2023-09-29 11:27:10 -0300

    dessenvolvimento

---
+++ script.js
@@ -0,0 +1,16 @@
+
+var isValid = false;
+
+while(radio.checked){
+    if(document.getElementsByClassName('certo').checked) {
+      
+      }else if(document.getElementsByClassName('errado').checked) {
+      
+      }
+}
+
+
+
+
+//var documento  = document.getElementsByClassName('certa');
+ 
